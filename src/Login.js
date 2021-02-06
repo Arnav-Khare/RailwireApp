2bd73884888ca3ee28a14c966d3d0fb1a31ca6fb
Arnav Khare <hashiraflame30@gmail.com>
2021-02-06 22:59:20 +0530

    Login Screen updated

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Dimensions, StyleSheet, Text, TouchableWithoutFeedback, View, TextInput } from 'react-native';
+import { Dimensions, StyleSheet, Text, TouchableWithoutFeedback,Button, View, TextInput } from 'react-native';
 
 const deviceHeight = Dimensions.get('window').height;
 const deviceWidth = Dimensions.get('window').width;
@@ -8,8 +8,8 @@ const deviceWidth = Dimensions.get('window').width;
 
 
 const Login = () => {
-        const [valueUsername, onChangeUsername] = React.useState('Useless Placeholder');
-        const [valuePassowrd, onChangepassword] = React.useState('Useless Placeholder');
+        const [valueUsername, onChangeUsername] = React.useState('');
+        const [valuePassowrd, onChangepassword] = React.useState('');
       
   return (      
     <View style={styles.container}>
@@ -17,24 +17,32 @@ const Login = () => {
             <Text style = {styles.headingText}>Data Usage Tracker </Text>
         </View>
         <View style= {styles.loginSetup}>
-        <View style = {{position:"relative",marginTop:230,}}>
-            <Text style = {{paddingTop:0,borderWidth:1}}>Username:</Text>
-            <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 2 ,borderRadius:40,}}
-            onChangeText={text => onChangeUsername(text)}
-            value={valueUsername}
-            />
-
-            <Text>Password:</Text>
-            <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 2 ,borderRadius:40,}}
-            onChangeText={text => onChangepassword(text)}
-            value={valuePassowrd}
-            />
+            <View >
+                <Text style = {styles.usernameTitle}>Username:</Text>
+                <TextInput
+                style={styles.usernameInput}
+                onChangeText={text => onChangeUsername(text)}
+                value={valueUsername}
+                placeholder="Enter Username"
+                />
+            </View>
+             <View >
+                <Text style={styles.passwordTitle}>Password:</Text>
+                    <TextInput
+                    style={styles.passwordInput}
+                    onChangeText={text => onChangepassword(text)}
+                    value={valuePassowrd}
+                    placeholder="Enter Password..!"
+                    />  
             </View> 
+            <Button
+            title="Learn More"
+            color="lightblue"
+            accessibilityLabel="Learn more about this purple button"
+            />
         </View>
         <View style = {styles.footer}>
-            <Text>Made by Arnav-Khare</Text>
+            <Text style={{marginLeft:121}}>Made by Arnav-Khare</Text>
         </View>
     </View> 
   );
@@ -51,24 +59,49 @@ const styles = StyleSheet.create({
       marginTop:20,
       width:deviceWidth,
       flex:1,
-      borderWidth:1
+      justifyContent:'center',
+      paddingLeft:44
   },
   headingText :{
       color:"black",
-      fontSize: 30,
-      
+      fontSize: 30, 
   },
   loginSetup: {
       flex:3,
-      borderWidth:2,
       borderColor: "blue",
       height: 0,
       width: deviceWidth ,
-
   },
+  usernameTitle: {
+      marginTop:74,
+      marginLeft:151,
+      alignItems:'center',
+      fontWeight:"400",
+  },
+  usernameInput:{
+      marginTop:11,
+      marginLeft:71,
+      borderWidth:2,
+      width:211,
+      height:50,
+      paddingLeft:11,
+  },  
+  passwordTitle: {
+    marginTop:44,
+    marginLeft:151,
+    alignItems:'center',
+    fontWeight:"400",
+},
+  passwordInput:{
+    marginTop:11,
+    marginLeft:71,
+    borderWidth:2,
+    width:211,
+    height:50,
+    paddingLeft:11,
+},
   footer : {
       flex:0.5,
-      borderWidth:2,
   }
 })
 
